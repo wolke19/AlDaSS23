a10186cafe52011cbc71786fcce7777a2e3d738b
Lars Roloff <la851rol@htwg-konstanz.de>
2023-04-11 23:18:03 +0200

    started vis of postfix calc. static base elements done

--- Aufgabe02/PostfixCalculator.java
+++ Aufgabe02/PostfixCalculator.java
@@ -4,23 +4,24 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 import java.util.Stack;
 
-public class PostfixToInfix {
+public class PostfixCalculator {
     public static void main(String[] args) throws IOException {
         Stack<Double> postfixStack = new Stack<Double>();
         Path inputPath = Paths.get("Aufgabe02/PostfixInput.txt");
-
         Scanner fileScanner = new Scanner(inputPath);
+
         while (fileScanner.hasNext()){
             String temp = fileScanner.next();
 
             switch (temp){
-                case "+" -> postfixStack.push(postfixStack.pop() + postfixStack.pop()) ;
+                case "+" -> postfixStack.push(postfixStack.pop() + postfixStack.pop());
                 case "*" -> postfixStack.push(postfixStack.pop() * postfixStack.pop());
-                case "-" -> postfixStack.push(-postfixStack.pop() + postfixStack.pop());
+                case "-" -> postfixStack.push(- postfixStack.pop() + postfixStack.pop());
                 case "/" -> postfixStack.push(Math.pow((postfixStack.pop() / postfixStack.pop()), -1));
                 default -> postfixStack.push(Double.parseDouble(temp));
             }
         }
+
         System.out.println("Result: " + postfixStack.pop());
         fileScanner.close();
     }
