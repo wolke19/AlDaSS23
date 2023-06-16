a8642044282f66738998440d27fc911e417168c5
Lars Roloff <la851rol@htwg-konstanz.de>
2023-06-16 10:32:13 +0200

    hashlogger now in main

--- Aufgabe05/Hash.java
+++ Aufgabe05/Hash.java
@@ -2,12 +2,4 @@ public class Hash {
     public static int hashHTWGUsername(String email){
         return email.trim().split("@")[0].chars().sum() * 13;
     }
-    public static void hashHTWGUsernameWithLogs(String email){
-        System.out.println("______ LOGS __________________________________________________________");
-        String username = email.trim().split("@")[0];
-        System.out.println("username: " + username);
-        username.chars().forEach(ch -> System.out.println((char) ch + ":" + ch));
-        System.out.println("Summe: " + username.chars().sum());
-        System.out.println("x13: " + username.chars().sum() * 13);
-    }
 }
--- Aufgabe05/Main.java
+++ Aufgabe05/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         String testEmail = "test888test@htwg-konstanz.de";
         System.out.println(Hash.hashHTWGUsername(testEmail));
-        Hash.hashHTWGUsernameWithLogs(testEmail);
+        hashHTWGUsernameWithLogs(testEmail);
 
         System.out.println();
         System.out.println();
@@ -20,5 +20,14 @@ public class Main {
         containerMap.addEdge(5,6,5);
         System.out.println("GRAPH READY.........");
         containerMap.printGraph();
+    }
+
+    public static void hashHTWGUsernameWithLogs(String email){
+        System.out.println("______ LOGS __________________________________________________________");
+        String username = email.trim().split("@")[0];
+        System.out.println("username: " + username);
+        username.chars().forEach(ch -> System.out.println((char) ch + ":" + ch));
+        System.out.println("Summe: " + username.chars().sum());
+        System.out.println("x13: " + username.chars().sum() * 13);
     }
 }
